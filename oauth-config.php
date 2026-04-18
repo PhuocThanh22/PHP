412bb33f5fe5bebba1cbca76226a8938099249bd
Phuoc Thanh <tranvanphuocthanh2106@gmail.com>
2026-04-18 09:14:00 +0700

    chore: sync current local code

--- oauth-config.php
+++ oauth-config.php
@@ -2,13 +2,13 @@
 
 return [
     // Google OAuth credentials
-    'GOOGLE_OAUTH_CLIENT_ID' => '',
-    'GOOGLE_OAUTH_CLIENT_SECRET' => '',
+    'GOOGLE_OAUTH_CLIENT_ID' => getenv('GOOGLE_OAUTH_CLIENT_ID') ?: '',
+    'GOOGLE_OAUTH_CLIENT_SECRET' => getenv('GOOGLE_OAUTH_CLIENT_SECRET') ?: '',
     'GOOGLE_OAUTH_REDIRECT_URI' => 'http://localhost:8888/phuocthanh/PHPCHINH/index.php?api=social_oauth_callback&provider=google',
 
     // Facebook OAuth credentials
-    'FACEBOOK_OAUTH_CLIENT_ID' => '',
-    'FACEBOOK_OAUTH_CLIENT_SECRET' => '',
+    'FACEBOOK_OAUTH_CLIENT_ID' => getenv('FACEBOOK_OAUTH_CLIENT_ID') ?: '',
+    'FACEBOOK_OAUTH_CLIENT_SECRET' => getenv('FACEBOOK_OAUTH_CLIENT_SECRET') ?: '',
     'FACEBOOK_OAUTH_REDIRECT_URI' => 'http://localhost:8888/phuocthanh/PHPCHINH/index.php?api=social_oauth_callback&provider=facebook',
 
     // SMTP email (for OTP verify code)
@@ -18,9 +18,9 @@ return [
     'SMTP_HOST' => 'smtp.gmail.com',
     'SMTP_PORT' => '587',
     'SMTP_SECURE' => 'tls',
-    'SMTP_USERNAME' => '',
-    'SMTP_PASSWORD' => '',
-    'SMTP_FROM_EMAIL' => '',
+    'SMTP_USERNAME' => getenv('SMTP_USERNAME') ?: '',
+    'SMTP_PASSWORD' => getenv('SMTP_PASSWORD') ?: '',
+    'SMTP_FROM_EMAIL' => getenv('SMTP_FROM_EMAIL') ?: '',
     'SMTP_FROM_NAME' => '3 CHU CUN CON',
     'SMTP_TIMEOUT' => '15',
 ];
